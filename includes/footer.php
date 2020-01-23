1e637f7f61ba89ec549eac627c9d1f82cf95a8c0
swaraj-a-s <37441725+swaraj-a-s@users.noreply.github.com>
2020-01-23 23:03:37 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">BIT, 2019</p>
+      </div>
+      <!-- /.container -->
+    </footer>
